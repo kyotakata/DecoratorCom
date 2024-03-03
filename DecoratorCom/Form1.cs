1e177f8f63ebce804954908f8783b8b6d3988eda
kyotakata <57854222+kyotakata@users.noreply.github.com>
2024-03-04 07:25:50 +0900

    55. E02_通信処理部分の実装

--- DecoratorCom/Form1.cs
+++ DecoratorCom/Form1.cs
@@ -1,3 +1,5 @@
+using DecoratorCom.Messages;
+
 namespace DecoratorCom
 {
     public partial class Form1 : Form
@@ -5,6 +7,14 @@ namespace DecoratorCom
         public Form1()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var measureId = Convert.ToInt32(textBox1.Text);
+            var message = new MeasureMessage(measureId);
+            listBox1.Items.Add(string.Join("-", message.GetBytes()));
         }
     }
 }
---
+++ DecoratorCom/Messages/IMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorCom.Messages
+{
+    public interface IMessage
+    {
+        IEnumerable<byte> GetBytes();
+    }
+}
